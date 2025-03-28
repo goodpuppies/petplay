560faf056f6359cdfa3f893f1fd7a207ea35b409
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2025-03-28 21:36:28 +0200

    no debug

--- petplay/init.ts
+++ petplay/init.ts
@@ -4,7 +4,7 @@ import { IrohWebWorker, setupIrohDebugMode } from "../submodules/irohworker/Iroh
 setupIrohDebugMode(false);
 const postalservice = new PostalService(IrohWebWorker);
 
-PostalService.debugMode = true;
+PostalService.debugMode = false;
 postalservice.initSignalingClient("ws://petplay.ddns.net:8080");
 
 const mainAddress = await postalservice.add("./main.ts");
